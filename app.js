15c23097ec955eb99fb2e1d8f55736b51910bf95
sharif-hossain <sharifcsekuet@gmail.com>
2020-08-03 19:26:54 +0600

    playing with colors

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ['purple', 'blueViolet', 'deepPink', 'tomato', 'blue', 'green', 'red', 'rgba(133,122,200)', '#f15025', '#ABCDEF'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    let colorRandom = Math.floor(Math.random() * colors.length);
+    return colorRandom;
+
+}
